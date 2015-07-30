4b4da3be30a21450c320572bafaff2f39b076489
jacktone <74020896@qq.com>
2015-07-30 16:22:22 +0800

    第一次本地库上传。

---
+++ test/src/com/example/test/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+    private class test{
+    	private String name;
+    	private int age;
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+    	
+    }
+}
